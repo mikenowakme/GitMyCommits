9f44010d5613ca81004db9d2b10b14d72ef548a5
Mike Nowak <mike@mikenowak.me>
2020-12-10 20:45:24 -0500

    Initial Commit

---
+++ GitMyCommits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitMyCommits
+//
+//  Created by Mike Nowak on 12/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitMyCommits/GitMyCommitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitMyCommitsApp.swift
+//  GitMyCommits
+//
+//  Created by Mike Nowak on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct GitMyCommitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
